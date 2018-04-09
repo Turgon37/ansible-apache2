83eb2a9e3ed85e617edb184ad3cfdde0e5f44779
Pierre GINDRAUD <pgindraud@gmail.com>
2018-04-10 00:16:04 +0200

    add virtual host handling

---
+++ files/apache2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import json
+import os
+import re
+import subprocess
+import sys
+
+content=dict()
+
+# 6.6.1p1 style formatting
+# 7.2p2 style formatting
+version_re = re.compile('^Server\s*version:\s*Apache/(?P<version>(?P<major>[0-9]+)[^ ]+)')
+try:
+    result = subprocess.Popen(['/usr/bin/env', 'apache2', '-v'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    (stdout, stderr) = result.communicate()
+    match = version_re.search(stdout)
+    if match:
+        content['version_full'] = match.group('version')
+        content['version_major'] = match.group('major')
+except subprocess.CalledProcessError as e:
+    content['error'] = str(e)
+
+if len(content) == 0:
+    content = None
+
+print(json.dumps(content))
+sys.exit(0)
